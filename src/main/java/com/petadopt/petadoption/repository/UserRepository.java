4857c5c3248bda06f13ed81549c79f7bf8bca395
noahrose82 <73205797+noahrose82@users.noreply.github.com>
2025-06-05 19:38:51 -0700

    Delete src directory

--- src/main/java/com/petadopt/petadoption/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-/*package com.petadopt.petadoption.repository;
-
-import com.petadopt.petadoption.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}*/

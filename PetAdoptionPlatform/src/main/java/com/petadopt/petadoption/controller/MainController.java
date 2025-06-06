c9c2f69741b27ef3e525ca5eb6dc67c4cf058af9
noahrose82 <73205797+noahrose82@users.noreply.github.com>
2025-06-05 19:38:19 -0700

    update

--- PetAdoptionPlatform/src/main/java/com/petadopt/petadoption/controller/MainController.java
+++ PetAdoptionPlatform/src/main/java/com/petadopt/petadoption/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.petadopt.petadoption.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
     
     @GetMapping
-    @ResponseBody
-        public String getMethodName() {
-            return "test";
-        }
+    public String display(Model model) {
+            
+        return "index";
+    }
+    
+    @GetMapping("/loggedin")
+    public String displayLoggedIn(Model model) {
+        return "index";
+    }
 }
--- PetAdoptionPlatform/src/main/java/com/petadopt/petadoption/controller/UserController.java
+++ PetAdoptionPlatform/src/main/java/com/petadopt/petadoption/controller/UserController.java
@@ -3,23 +3,42 @@ package com.petadopt.petadoption.controller;
 import com.petadopt.petadoption.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    
-    
 
     @GetMapping("/register")
-    public String display(Model model) {
-        model.addAttribute("title", "Registration Form");
+    public String displayRegistration(Model model) {
         model.addAttribute("user", new User());
         return "register";
     }
 
-/*    @PostMapping("/register")
-    public String processRegistration(@ModelAttribute User user) {
-        return "redirect:/login";
-    }*/
+   	@PostMapping("/doRegister")
+    public String doRegister(User user, BindingResult bingingResult, Model model) {
+        
+        return "redirect:/user/login";
+    }
+    
+    @GetMapping("/login")
+    public String displayLogin(Model model) {
+        model.addAttribute("loginUser", new User());
+        return "login";
+    }
+    
+    @PostMapping("/doLogin")
+    public String doLogin(User user, BindingResult bingingResult, Model model) {
+        String username = "a";
+        String password = "1";
+
+        if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+            return "redirect:/dashboard/loggedin";
+        }
+        
+        else {
+        	return "redirect:/user/login";
+        }
+    }
 }
